6c3f4f83ea4ec19da51c3ca04077012c1948d914
Pop Marius <popmarius86@gmail.com>
2017-10-02 21:22:59 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+
+define("APP_ID", "407217");
+define("APP_KEY", "5e613cf066db77ef9bd6");
+define("APP_SECRET", "9f6e3c0858e2dfa0144a");
+define("APP_CLUSTER", "eu");
+?>
