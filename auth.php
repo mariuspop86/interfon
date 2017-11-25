acd5fce64a3533f64337705b5c1bf191390a1f38
Marius <marius.pop@pitechnologies.ro>
2017-11-25 10:51:01 +0200

    auth php

--- auth.php
+++ auth.php
@@ -12,4 +12,7 @@ require __DIR__ . '/vendor/autoload.php';
 $pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID, array('cluster' => APP_CLUSTER));
 
 $presence_data = array('name' => $_POST['name']);
+
+$pusher->trigger($_POST["channel_name"], "message", $_POST["message"], $_POST["socketId"], true);
+
 echo $pusher->presence_auth($_POST['channel_name'], $_POST['socket_id'], $_POST['id'], $presence_data);
